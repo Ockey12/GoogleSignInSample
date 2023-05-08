80732ecfa04dfb79670fd23cdfe2cfd02c07a09a
OnagaHaruki <haloupgarou@gmail.com>
2023-05-08 16:44:58 +0900

    Initial Commit

---
+++ GoogleSignInSample/GoogleSignInSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSignInSampleApp.swift
+//  GoogleSignInSample
+//
+//  Created by オナガ・ハルキ on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSignInSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
